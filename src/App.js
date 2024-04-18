04b82e19c447efb35586fbdbe014315d600a2862
sadacloud24 <167418216+sadacloud24@users.noreply.github.com>
2024-04-18 15:08:02 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <div className='App-header'>
+    <h2>Header</h2>
+    </div>
+
+    <div className='body'>
+      <h2>Wel Come Azure cloud!</h2>
+    </div>
+    </>
+  );
+}
+
+export default App;
